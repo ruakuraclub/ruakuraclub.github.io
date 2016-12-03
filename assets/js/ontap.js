9f7a2946320098d631f21c4e7db174a375ee7291
Brad Cowie <brad@gizmoguy.net.nz>
2016-12-04 11:26:39 +1300

    Update ontap page to use clubfare API instead of iframe.

---
+++ assets/js/ontap.js
@@ -0,0 +1,47 @@
+function makeTapList(beers){
+	var locationclass = {
+		6 : "serving",
+		7 : "low",
+		8 : "next"
+	};
+
+	$('#taplist ul').empty();
+	$.each(beers, function(key, beer){
+		element = "";
+		element += '<li class=\"'+locationclass[beer.location.id]+'\">';
+		element += '<h2>'+beer.brewer.name+' <em>'+beer.name+'</em></h2>';
+		element += beer.style.name+' &mdash;';
+		element += '<strong>'+beer.abv+'% ABV</strong><br>';
+		element += beer.brewer.location+'<br>';
+		element += '<p></p></li>';
+		$('#taplist ul').append(element);
+	});
+}
+
+var ready = function() {
+
+	$.ajax('http://clubfare.hs.net.nz/api/beers', {
+		success: function (data, textStatus, xhr) {
+			makeTapList(data);
+		}
+	});
+
+	var etag = null;
+	setInterval(function () {
+		$.ajax('http://clubfare.hs.net.nz/api/beers', {
+			success: function (data, textStatus, xhr) {
+				if (etag == null) {
+					etag = xhr.getResponseHeader('Etag');
+				}
+
+				if (xhr.getResponseHeader('ETag') != etag) {
+					// Page needs refreshing because etag has changed
+					makeTapList(data);
+				}
+			}
+		});
+	}, 10000);
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
