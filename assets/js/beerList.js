b5b9944c81e91ad1d32fd75ed0c53ba5aec166b9
Andrew Clow <andrew@freedomsoftware.co.nz>
2019-12-30 20:19:43 +1300

    :construction::fire::hammer:Redesign with bootstrap-4
    Beginning a modernisation of the site with responsive design. Still a work-in-progress.

---
+++ assets/js/beerList.js
@@ -0,0 +1,74 @@
+function makeTapList(beers){
+	var locationclass = {
+		6 : "serving",
+		7 : "low",
+		8 : "next"
+	};
+
+	$('#taplist').empty();
+	$.each(beers, function(key, beer){
+		/*if ( beer.location.status.toLowerCase() == 'serving' ) {*/
+			if (!beer.note.trim()) {
+				element = "<div class=\"card col-md-4 flex-column\">";
+			} else {
+				element = '<div class=\"card col-md-4 flex-column\" data-toggle="tooltip" title="'+beer.note+'">';
+			}
+			element += '<div class=\"card-body\">';
+			element += '<h5 class=\"card-title\">'+beer.brewer.name+'</h5>';
+			element += '<h4 class=\"card-subtitle mb-2 text-muted\">'+beer.name+'</h4>';
+			element += '<p class=\"card-text\">'+beer.style.name+' &mdash; ';
+			element += '<strong>'+beer.abv+'% ABV</strong></p>';
+			element += '<p class=\"text-muted\">'+beer.brewer.location;
+			element += '<img class=\"float-right\" src=\"./assets/images/'+locationclass[beer.location.id]+'.png\"></img>';
+			element += '</p></div>';
+			$('#taplist').append(element);
+		/*}*/
+	});
+
+	$('[data-toggle="tooltip"]').tooltip({
+		'placement': 'right'
+	});
+}
+
+var ready = function() {
+
+	$.ajax('https://clubfare.hs.net.nz/api/beers', {
+		success: function (data, textStatus, xhr) {
+			data.sort(function(a, b){
+				var status1 = a.location.status.toLowerCase();
+				var status2 = b.location.status.toLowerCase();
+			  var brewer1 = a.brewer.name.toLowerCase();
+			  var brewer2 = b.brewer.name.toLowerCase();
+			  var beer1 = a.name.toLowerCase();
+			  var beer2 = b.name.toLowerCase();
+			  if (status1 < status2) {return 1;}
+			  if (status1 > status2) {return -1;}
+			  if (brewer1 < brewer2) {return -1;}
+			  if (brewer1 > brewer2) {return 1;}
+			  if (beer1 < beer2) {return -1;}
+			  if (beer1 > beer2) {return 1;}
+			  return 0;
+			});
+			makeTapList(data);
+		}
+	});
+
+	var etag = null;
+	setInterval(function () {
+		$.ajax('https://clubfare.hs.net.nz/api/beers', {
+			success: function (data, textStatus, xhr) {
+				if (etag == null) {
+					etag = xhr.getResponseHeader('Etag');
+				}
+
+				if (xhr.getResponseHeader('ETag') != etag) {
+					// Page needs refreshing because etag has changed
+					makeTapList(data);
+				}
+			}
+		});
+	}, 10000);
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
